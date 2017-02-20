5d2bb4fe077ab79182eddd4d8795c8da57dde34f
Daniel Jones <messyjones@gmail.com>
2017-02-19 22:06:11 -0500

    got TA help with the homework

--- Homeworks/Hmwk 2/bieber.js
+++ Homeworks/Hmwk 2/bieber.js
@@ -1,78 +1,45 @@
-//$.ajax({
-//
-//  // The 'type' property sets the HTTP method.
-//  // A value of 'PUT' or 'DELETE' will trigger a preflight request.
-//  type: 'GET',
-//
-//  // The URL to make the request to.
-//  url: 'http://html5rocks-cors.s3-website-us-east-1.amazonaws.com/index.html',
-//
-//  // The 'contentType' property sets the 'Content-Type' header.
-//  // The JQuery default for this property is
-//  // 'application/x-www-form-urlencoded; charset=UTF-8', which does not trigger
-//  // a preflight. If you set this value to anything other than
-//  // application/x-www-form-urlencoded, multipart/form-data, or text/plain,
-//  // you will trigger a preflight request.
-//  contentType: 'text/plain',
-//
-//  xhrFields: {
-//    // The 'xhrFields' property sets additional fields on the XMLHttpRequest.
-//    // This can be used to set the 'withCredentials' property.
-//    // Set the value to 'true' if you'd like to pass cookies to the server.
-//    // If this is enabled, your server must respond with the header
-//    // 'Access-Control-Allow-Credentials: true'.
-//    withCredentials: false,
-//    "Access-Control-Allow-Origin" : "*"
-//  },
-//
-//  headers: {
-//    // Set any custom headers here.
-//    // If you set any non-simple headers, your server must include these
-//    // headers in the 'Access-Control-Allow-Headers' response header.
-//  },
-//
-//  success: function() {
-//    // Here's where you handle a successful response.
-//  },
-//
-//  error: function() {
-//    // Here's where you handle an error response.
-//    // Note that if the error was due to a CORS issue,
-//    // this function will still fire, but there won't be any additional
-//    // information about the error.
-//  }
-//});
-//
-//function loadDoc() {
-//    console.log("in the load doc func")
-//    setInterval(makeRequest, 3000);
-//}
-//
-//function makeRequest() {
-//    console.log("making a request");
-//     $.get('http://www.example.com/content.json', function(data, status) {
-//      if (status === "success") {
-//         // do something with data
-//      } else {
-//         // something went wrong, check status
-//      }
-//   });
-//}
-//
-//function setHeader(xhr) {
-//  xhr.setRequestHeader('Authorization', token);
-//}
+var URL = 'http://ec2-54-210-131-157.compute-1.amazonaws.com/feed/:djones14'
+
+
+
+function makeRequest(URL, callback) {
+    console.log("making a request");
+     $.get(URL, function(data, status) {
+      if (status === "success") {
+         // do something with data
+          console.log(data);
+      } else {
+         // something went wrong, check status
+      }
+   });
+}
+
 function handleClick(cb) {
     console.log("clicked!!!!!");
+//    makeRequest();
     addTweet();
 }
 
 function addTweet() {
-    var stream = document.getElementById("stream");
-    var tweet = document.getElementsByClassName("tweet");
-    console.log(tweet[0]);
-    stream.appendChild(tweet[0]);
-//   stream.insertBefore(tweet, tweet);
+     var stream = $('#stream');
+    var str = 'hi'
+    var name = $('<p>' + str + '</p>');
+    
+
+//    <div class="tweet">
+//            <div class="content"> <img class="avatar" src="img/damenleeturks.jpg" /> 
+//                <strong class="fullname">My BFF</strong> 
+//                <span class="username">@mybff</span>
+//                <p class="tweet-text">Today is an amazing day.</p>
+//            </div>
+//    </div>
+                
+    var tweet = $("<p>hi</p>").addClass("content");
+    stream.append(tweet);
+    
+//    name.append()
+//    
+//     stream.append($('<p>' + str + '<p>').addClass("parry"))
     
     console.log(tweet);
 }
