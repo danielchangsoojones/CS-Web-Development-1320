7de47839eb80d636b8f5fa13ee6991f54ea91bd1
Daniel Jones <messyjones@gmail.com>
2017-03-12 19:39:44 -0400

    added a read file

---
+++ Homeworks/Hmwk 3/readMe.js
@@ -0,0 +1,3 @@
+//README:
+//I used the node.js Udemy course by Rob Percival to help my understand how to use socket.io in a chat application. Socket.io automatically updates messages rather than running a timer that runs queries. 
+//I also used sequelize to help me run sql queries. Sequelize simplifies the syntax of sql. 
